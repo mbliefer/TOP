32d61bfd3b0b37dbe0288f12484839b53015b1ad
mbliefer <mblief@gmail.com>
2023-06-13 20:56:36 -0500

    remove from array

--- library/script.js
+++ library/script.js
@@ -6,13 +6,18 @@ const books = document.querySelector('#books');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
-const notRead = document.querySelector('#not-read');
 const read = document.querySelector('#read');
 const readCheck = document.querySelector('#read-check');
 const bookCards = document.getElementsByClassName('book-card');
-// const trash = document.getElementsByClassName('trash');
-const trash = document.querySelector('.trash');
+const trash = document.getElementsByClassName('trash');
 
+let div;
+let h2;
+let h3;
+let h4;
+let label;
+let input;
+let img;
 
 let newBook;
 let myLibrary = [];
@@ -22,53 +27,19 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    // this.info = function() {
-    //     return `${title} ${author} ${pages} ${read}`
-    // }
+}
+
+Book.prototype.readStatus = function () {
+
 }
 
 Book.prototype.createBookCard = function () {
-    const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const h3 = document.createElement('h3');
-    const h4 = document.createElement('h4');
-    const label = document.createElement('label');
-    const input = document.createElement('input');
-    const img = document.createElement('img');
-    img.src = 'images/red-delete-10437.svg';
-    img.classList.add('trash');
-    div.classList.add('book-card');
-    h2.classList.add('title');
-    h3.classList.add('author');
-    h4.classList.add('pages');
-    label.htmlFor = "read-check";
-    label.textContent = "Read ";
-    input.type = "checkbox";
-    input.id = "read-check";
-    input.name = "status";
-    input.value = "Read";
-
-    // const trashCans = Array.from(trash);
-    // trashCans.forEach((can) => {
-    //     can.addEventListener('click', () => {
-    //         console.log(can);
-    //         console.log(this);
-    //     })
-    // });
-
+    createCardElements();
 
     h2.textContent = this.title;
     h3.textContent = this.author;
     h4.textContent = this.pages;
     input.checked = newBook.read;
-
-    books.appendChild(div);
-    div.appendChild(h2);
-    div.appendChild(h3);
-    div.appendChild(h4);
-    div.appendChild(label);
-    div.appendChild(input);
-    div.appendChild(img);
 }
 
 
@@ -81,13 +52,60 @@ function addBook() {
     if (read.checked) {
         readInput = true;
     }
+    else {
+        readInput = false;
+    };
 
     newBook = new Book(titleInput, authorInput, pageInput, readInput);
     myLibrary.push(newBook);
     newBook.createBookCard();
 }
 
-const displayBook = () => console.log('hi');
+function createCardElements() {
+    div = document.createElement('div');
+    h2 = document.createElement('h2');
+    h3 = document.createElement('h3');
+    h4 = document.createElement('h4');
+    label = document.createElement('label');
+    input = document.createElement('input');
+    img = document.createElement('img');
+    img.src = 'images/red-delete-10437.svg';
+
+    img.classList.add('trash');
+    div.classList.add('book-card');
+    h2.classList.add('title');
+    h3.classList.add('author');
+    h4.classList.add('pages');
+
+    label.htmlFor = "read-check";
+    label.textContent = "Read ";
+    input.type = "checkbox";
+    input.id = "read-check";
+    input.name = "status";
+    input.value = "Read";
+
+    img.onclick = removeBook;
+
+    books.appendChild(div);
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(h4);
+    div.appendChild(label);
+    div.appendChild(input);
+    div.appendChild(img);
+}
+
+function removeBook(e) {
+    let trashCans = Array.from(trash);
+    e.target.parentNode.remove();
+    console.log(trashCans.indexOf(e.target));
+    myLibrary.forEach((book) => {
+        let bookToRemove = myLibrary.indexOf(book);
+        if (bookToRemove === trashCans.indexOf(e.target)) {
+            myLibrary.splice(bookToRemove, 1);
+        };
+    });
+}
 
 submitBook.addEventListener('click', (e) => {
     e.preventDefault();
@@ -105,4 +123,4 @@ closeX.addEventListener('click', () => {
     bookEntryForm.classList.remove('show');
 });
 
-trash.addEventListener('click', displayBook);
+
